bcde738ffcb0f1fb3c7338ceba76eb21e29e8b6b
alexandrevg54 <alexandrevg54@educastur.es>
2023-12-04 20:57:04 +0100

    fin del examen

---
+++ src/prueba/Ejercicio1.java
@@ -0,0 +1,15 @@
+package prueba;
+
+public class Ejercicio1 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	System.out.println("línea número");
+	readLine();
+	
+		}
+
+	}
---
+++ src/prueba/Ejercicio2.java
@@ -0,0 +1,9 @@
+package prueba;
+
+public class Ejercicio2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+}
+}
